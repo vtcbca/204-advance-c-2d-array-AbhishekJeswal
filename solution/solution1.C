e78f5960bd119554a8eb0aede081cfd04efe13a1
Abhishek Jeswal <100262141+AbhishekJeswal@users.noreply.github.com>
2022-03-12 12:27:23 +0530

    Add files via upload

---
+++ solution/solution1.C
@@ -0,0 +1,64 @@
+/*write a program to enter two 3*3 matrix and print and print the sum ofit.
+name:Jeswal Abhishek*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[3][3],b[3][3],c[3][3],i,j;
+	clrscr();
+	printf("\n\nenter values in first matrix :\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("\tenter value for element a[%d][%d] :",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	clrscr();
+	printf("\n\nenter values in second matrix :\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("\tenter value for element b[%d][%d] :",i,j);
+			scanf("%d",&b[i][j]);
+		}
+	}
+	clrscr();
+	printf("\nyour first matrix a is:\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf(" %d",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nyour second matrix b is:\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf(" %d",b[i][j]);
+		}
+		printf("\n");
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			c[i][j]=a[i][j]+b[i][j];
+		}
+	}
+	printf("\nsum of two matrix a+b is:\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf(" %d",c[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+}
